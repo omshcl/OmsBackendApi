74a008b4d42d2890ae1487f79c2b4240974955ee
Vandana Ganesh <vandana.ganesh@hcl.com>
2019-06-17 14:17:26 -0500

    added httpclient file

---
+++ src/main/java/oms/orders/OrderGet.java
@@ -0,0 +1,43 @@
+package oms.orders;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+@WebServlet(urlPatterns="/orders/getinfo")
+
+public class OrderGet extends HttpServlet {
+	
+	private OrderApi orderApi;
+	
+	public OrderGet() {
+		orderApi = new OrderApi();
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		//get data from request
+		BufferedReader reader = request.getReader();
+		StringBuffer jb = new StringBuffer();
+		String line = null;
+		while((line = reader.readLine()) != null)
+			jb.append(line);
+		
+		//parse request data and parse as json
+		JSONObject json = new JSONObject(jb.toString());
+		
+		//get id number from json
+		int id = json.getInt("id");
+
+		response.setCharacterEncoding("UTF-8");
+		response.setHeader("Access-Control-Allow-Origin","*");
+		String responseJson = orderApi.getSummary(id).toString();
+		response.getWriter().write(responseJson);
+	}
+	
+}
