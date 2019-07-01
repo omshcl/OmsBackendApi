7dec9e4d086a44600c75948695fb11f95cf2ab2e
Alice Reuter <alice@alicereuter.com>
2019-07-01 14:52:48 -0500

    created auto populate date for items table

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM alpine
 
-RUN apk add curl openjdk8 maven python bash
-RUN ls
+RUN apk add curl openjdk8 maven python bash py2-pip
+RUN pip install requests
 # install cassandra and java runtime
 RUN cd && curl "http://apache.claz.org/cassandra/3.11.4/apache-cassandra-3.11.4-bin.tar.gz" > cassandra.tar.gz && tar xvf cassandra.tar.gz
 COPY . /root/
 EXPOSE 8080
-#ENTRYPOINT ["/root/setup.sh"]
+ENTRYPOINT ["/root/setup.sh"]
--- setup.sh
+++ setup.sh
@@ -3,33 +3,11 @@ echo "[cassandra] spawning cassandra"
 cd root/apache-cassandra-3.11.4/bin/ || exit
 nohup ./cassandra -R -f > cassandralog &
 echo "[cassandra] started"
-sleep 10
+sleep 16
 echo "[db] initializing database"
 python cqlsh.py --file=../../setup.txt
 echo "[db] created table columns"
 echo "[backend] starting tomcat services"
 cd ../../
-nohup mvn tomcat7:run &
-echo "DONE"
-sleep 40;
 echo "populating items"
-curl --header "Content-Type: application/json" \
-  --request POST \
-  --data '{
- productclass: "new",
- eta: "6/29/19",
- shipbydate: "6/27/19",
- type: "pipeline",
- quantity: 10,
- shippingaddress: "12345 Laptop Street",
- category: "Technology",
- locationname: "Austin",
- isreturnable: "true",
- itemdescription: "15 inch Lenovo 2019 laptop P52",
- manufacturername: "Lenovo",
- price: 1500,
- shortdescription: "Lenovo Laptop",
- subcategory: "Laptop",
- unitofmeasure: "Each"
-}' \
-  localhost:8080/supply/new
+python populate.py
