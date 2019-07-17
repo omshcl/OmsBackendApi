af9ceb9211d527d31ff3ddba799888dd4a1fda29
Vandana Ganesh <vandana.ganesh@hcl.com>
2019-07-17 15:36:57 -0500

    changed api endpoint:

--- src/main/java/oms/orders/Reserve.java
+++ src/main/java/oms/orders/Reserve.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-@WebServlet(urlPatterns="/reserve")
+@WebServlet(urlPatterns="/orders/reserve")
 
 public class Reserve extends HttpServlet {
 	
